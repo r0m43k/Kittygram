58f6a2cc088ec7590a7a3806c04a94df34e876d2
r0m43k <r0m43k@kali.org>
2025-12-11 05:21:12 -0500

    final update

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,20 +1,21 @@
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() in ("true")
 
-ALLOWED_HOSTS = ['*']
+hosts = os.getenv("ALLOWED_HOSTS", "*")
+ALLOWED_HOSTS = hosts.split(",")
 
-STATIC_ROOT = "/app/collected_static"
+STATIC_ROOT = "/app/static"
 
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
